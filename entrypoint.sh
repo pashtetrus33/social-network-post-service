f8ab941e72b95ae2680e87c748a91805469cd380
Pavel <pashtetrus@gmail.com>
2025-02-19 20:44:49 -0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-# Проверяем версию Java
 java -version
 
-# Запускаем приложение
 java -jar /apps/app.jar
--- src/main/java/ru/skillbox/social_network_post/service/KafkaService.java
+++ src/main/java/ru/skillbox/social_network_post/service/KafkaService.java
@@ -3,15 +3,9 @@ package ru.skillbox.social_network_post.service;
 
 import ru.skillbox.social_network_post.dto.KafkaDto;
 
-import java.util.UUID;
-
 public interface KafkaService {
 
     void newPostEvent(KafkaDto kafkaDto);
 
     void newCommentEvent(KafkaDto kafkaDto);
-
-    void deletedAccountEvent(UUID accountId);
-
-    void blockedAccountEvent(UUID accountId);
 }
