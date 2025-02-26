a124e98bd922e4b5346fbd4ea2b7c9c9cadb7f02
Pavel <pashtetrus@gmail.com>
2025-02-26 13:44:29 -0300

    refactoring

--- src/main/java/ru/skillbox/social_network_post/aspect/LoggingAspect.java
+++ src/main/java/ru/skillbox/social_network_post/aspect/LoggingAspect.java
@@ -15,7 +15,7 @@ import java.util.Arrays;
 @Slf4j
 public class LoggingAspect {
 
-    @Value("${custom.logging.enabled:false}")
+    @Value("${custom.logging.enabled}")
     private boolean loggingEnabled;
 
     @Value("${custom.logging.level:INFO}")
