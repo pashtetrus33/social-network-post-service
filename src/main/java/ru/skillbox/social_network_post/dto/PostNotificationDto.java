9339ceb201077ce12b6b09c737d6ee2e936eb333
Pavel <pashtetrus@gmail.com>
2025-03-18 14:13:38 -0300

    refactoring

--- src/main/java/ru/skillbox/social_network_post/dto/PostNotificationDto.java
+++ src/main/java/ru/skillbox/social_network_post/dto/PostNotificationDto.java
@@ -1,5 +1,6 @@
 package ru.skillbox.social_network_post.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -19,5 +20,6 @@ public class PostNotificationDto {
 
     private String title;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime publishDate;
 }
